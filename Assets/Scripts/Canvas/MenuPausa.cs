5d13764ca8e15054c2d32bebbaf2966f3de8abef
Kat <kat.valcarcel@live.com>
2022-06-12 16:53:47 -0500

    Menu Pausar Reanudad

--- Assets/Scripts/Canvas/MenuPausa.cs
+++ Assets/Scripts/Canvas/MenuPausa.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
+    private bool juegoPausado = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else Pausa();
+        }
+    }
     public void Pausa()
     {
+        juegoPausado = true;
         Time.timeScale =0f;
         menuPausa.SetActive(true);
         botonPausa.SetActive(false);
     }
     public void Reanudar()
     {
+        juegoPausado = false;
         Time.timeScale = 1f;
         menuPausa.SetActive(false);
         botonPausa.SetActive(true);
+    }
+    public void Reiniciar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Cerrar()
+    {
+        Debug.Log("Cerrando juego");
+        Application.Quit();
     }
 }
